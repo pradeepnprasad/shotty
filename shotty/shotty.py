4a9e6013b747f5f6632f20713ce8dc803243e672
pprasad <pprasad@collegeboard.org>
2018-12-04 21:16:15 -0500

    Initial commit

---
+++ shotty/shotty.py
@@ -0,0 +1,38 @@
+import boto3
+import click
+
+
+session = boto3.session.Session(profile_name='cb-playground-nonprod', region_name='us-east-1')
+
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instances():
+    "List EC2 instances"
+    for i in ec2.instances.all():
+        print (','.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+
+    return
+
+
+if __name__ == '__main__':
+    list_instances()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
